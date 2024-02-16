d09d53f195ab383e2ce216d71742fbc387c76134
Vitor Matheus <vitormatheusob@gmail.com>
2024-02-16 18:45:35 -0300

    Teste TechNation

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Teste_Tecnico_TechNation
-Teste técnico TechNation
+
+# Teste Técnico TechNation
+
+Teste realizado para demonstração de conhecimento direcionado a front-end.
+
+
+
+## Autor
+
+- [@vitor-matheus1999](https://github.com/vitor-matheus1999)
+
+- Nome Completo: Vitor Matheus de Oliveira Borges
+
+- [LinkedIn](https://www.linkedin.com/in/vitor-matheus-oliveira-borges-0339a020b/)
+
+- E-mail profissional:vitormatheusob@gmail.com
+
+## Versões das Ferramentas Utilizadas
+
+| Ferramenta  | Versão      | 
+| :---------- | :--------- |
+| Font Awesome Free | 5.0 | 
+| DataTable Jquery | 1.13.7 | 
+| Jquery | 3.7.1 | 
+| Admin LTE |  3.2  | 
+| Google Fonts | - | 
+| Bootstrap  |5.3.2 | 
+|  Chart JS  | 4.0| 
+
+## Rodando localmente
+
+Realizando o "git clone" do projeto e seguindo para a pasta "views", será possível visualizar as páginas ".html", clicando em um dos arquivos será possível acessar a site criado.
+
+
+
+
+
+
+## Apêndice
+
+A página criada não realiza nenhuma requisição utilizando ajax, porém a lógica para realizar a funcionalidade juntamente com o JQuery, está no código porém está comentada para que não houvesse conflitos.
+
+
+
+
--- js/notas_emitidas.js
+++ js/notas_emitidas.js
@@ -4,7 +4,7 @@ let filtro = {
     'valorTipoFiltro':''
 }
 
-let dadosJson = [{'id':'1','nome_pagador':'João Carvalho','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'10/05/2023','data_cobranca_nota':'11/05/2023','data_pagamento_nota':'11/05/2023','status_nota':'Pagamento realizado'},{'id':'2','nome_pagador':'Ana Julia Tomas','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'20/02/2023','data_cobranca_nota':'21/02/2023','data_pagamento_nota':'26/02/2023','status_nota':'Pagamento em atraso'},{'id':'3','nome_pagador':'Valdemar Santos','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'10/10/2023','data_cobranca_nota':'12/10/2023','data_pagamento_nota':'-','status_nota':'Cobrança realizada'}]
+let dadosJson = [{'id':'1','nome_pagador':'João Carvalho','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'10/05/2023','data_cobranca_nota':'11/05/2023','data_pagamento_nota':'11/05/2023','status_nota':'4'},{'id':'2','nome_pagador':'Ana Julia Tomas','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'20/02/2023','data_cobranca_nota':'21/02/2023','data_pagamento_nota':'26/02/2023','status_nota':'3'},{'id':'3','nome_pagador':'Valdemar Santos','documento_nota_fiscal':'123.456.789-12','documento_boleto_bancario':'123.456.789-12','data_emissao_nota':'10/10/2023','data_cobranca_nota':'12/10/2023','data_pagamento_nota':'-','status_nota':'2'}]
 
 
 $(document).ready((e)=>{
@@ -16,6 +16,7 @@ $(document).ready((e)=>{
         //     dataSrc:'',
         //     dataType:'json'
         // },
+        dom:'Pfrtip',
         data:dadosJson,
         columns:[
             {
@@ -49,12 +50,32 @@ $(document).ready((e)=>{
             {
                 data:'status_nota',
                 title:'Status',
-                render:{}
+                render:function(data,type){
+                    let status = '';
+                    switch(data){
+                        case '1':
+                            status = '<span class="badge badge-primary">Emitida</span>'
+                            break
+                        case '2':
+                            status = '<span class="badge badge-warning"> Cobrança realizada</span>'
+                            break
+                        case '3':
+                            status = '<span class="badge badge-danger">Pagamento em atraso</span>'
+                            break
+                        case '4':
+                            status = '<span class="badge badge-success">Pagamento realizado</span>'
+                            break
+                    }
+                    return status;
+                }
             }
         ],
         columnDefs:[
-            {className: "text-center", targets: "_all"}
-        ]
+            {
+                className: "text-center",
+                targets: "_all",
+            }
+        ],
     });
 
     $('#selectFiltro').on('change', (e)=>{
@@ -84,13 +105,32 @@ $(document).ready((e)=>{
     });
 
     $('#selectStatus').on('change',(e)=>{
-        let tipo_filtro = $('#selectFiltro').val();
         let status = $('#selectStatus').val();
+        switch(status){
+            case '1':
+                // dadosJson.forEach((e)=>{
+                //     if(e.status_nota != 1){
+                        
+                //     }
+                // })
 
-        filtro.tipoFiltro = tipo_filtro;
-        filtro.valorTipoFiltro = status;
+                for(var i = 0; i <= dadosJson.length; i++){
+                    
+                    
+                }
 
-        tableNotasEmitidas.ajax.reload();
+                console.log(dadosJson.status_nota)
+                break
+            case '2':
+                status = '<span class="badge badge-warning"> Cobrança realizada</span>'
+                break
+            case '3':
+                status = '<span class="badge badge-danger">Pagamento em atraso</span>'
+                break
+            case '4':
+                status = '<span class="badge badge-success">Pagamento realizado</span>'
+                break
+        }
     });
 
 
